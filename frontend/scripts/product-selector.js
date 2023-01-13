6e19d8051c1ff250ab2a5bad861d9f10eeff9f18
Lauge Milling <lm@comonnetwork.com>
2023-01-13 17:45:57 +0100

    select product variant

--- frontend/scripts/product-selector.js
+++ frontend/scripts/product-selector.js
@@ -1,5 +1,3 @@
-import { __isOptionsFunction } from "@tailwindcss/typography";
-
 const customOptions = document.querySelectorAll('form .custom-selector .variant input');
 const customLabels = document.querySelectorAll('form .custom-selector .variant label');
 const masterSelect = document.querySelector('#product-select');
@@ -16,4 +14,28 @@ customOptions.forEach(option => {
       label.classList.add('checked');
     }
   });
+});
+
+// Dropdown
+const customSelectWrapper = document.querySelector('form .product-variants.select');
+const customSelect = customSelectWrapper.querySelector('form .product-variants.select .select');
+const customDropdown = customSelectWrapper.querySelector('form .product-variants.select .dropdown');
+const customVariants = customDropdown.querySelectorAll('.variant[data-variant]');
+
+customVariants.forEach(variant => {
+  variant.addEventListener('click', function(e) {
+    let selectedVariant = e.currentTarget;
+    let selectedVariantID = selectedVariant.getAttribute('data-variant');
+
+    // Update master
+    masterSelect.value = selectedVariantID;
+
+    // Update view
+    customSelect.querySelector('.inner').innerHTML = selectedVariant.innerHTML;
+    customSelectWrapper.classList.remove('open');
+  });
+});
+
+customSelect.addEventListener('click', function(e) {
+  customSelectWrapper.classList.toggle('open');
 });
